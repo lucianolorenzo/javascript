3d3463ed582e2481401194f05878c2d720b2e6b7
lucianolorenzo <luciano.lorenzo17@csj-rpi.org>
2015-09-17 09:36:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+function changeimage() {
+  var image = document.getElementById('myImage'):
+  
+  if (image.src.match(bulbon")) {
+    image.src = "images/pic_bulboff.gif";
+} else {
+  image.src = "images/pic_bulbon.gif";
+  }
+}
